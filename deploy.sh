79b7336253c30e4f74eba7344070e3b17b8cbd3e
Marco Troisi <hello@marcotroisi.com>
2016-04-21 14:24:33 +0100

    Updating Site

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,9 @@ echo "*** Git add & commit ***"
 git add -A
 git commit -m "Updating Site" && git push origin master
 
+mkdir public/feed/
+cp public/index.xml public/feed/ 
+
 echo "*** Push Public folder to GitHub Pages ***"
 git subtree push --prefix=public https://github.com/marcotroisi/blog.git gh-pages
 
