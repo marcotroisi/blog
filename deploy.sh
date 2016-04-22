fa707c7c7680abe118d78ae70e4fe1e23be73acd
Marco Troisi <hello@marcotroisi.com>
2016-04-22 09:12:53 +0100

    Updating Site

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,10 @@ echo "RSS in /feed"
 mkdir public/feed/
 cp public/index.xml public/feed/
 
+echo "/tag folder"
+mkdir public/tag
+cp -r public/tags public/tag
+
 echo "*** Git add & commit ***"
 git add -A
 git commit -m "Updating Site" && git push origin master
