31063aaf167e268d5c537c58df27310644582536
Marco Troisi <hello@marcotroisi.com>
2024-03-30 12:59:09 +0000

    Updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,18 +15,21 @@ echo "/category folder"
 mkdir public/category
 cp -r public/categories/* public/category/
 
+echo "*** Push Public folder to GitHub Pages (in the /docs folder) ***"
+cp -r public docs
+
 
 echo "*** Git add & commit ***"
 git add -A
 git commit -m "Updating Site"
 git push origin master
 
-echo "*** Push Public folder to GitHub Pages ***"
-git push origin :gh-pages
-if [[ $GH_TOKEN ]]; then
-	git subtree push --prefix=public https://$GH_TOKEN@github.com/marcotroisi/blog.git gh-pages > /dev/null 2>&1
-else
-	git subtree push --prefix=public https://github.com/marcotroisi/blog.git gh-pages
-fi
+# echo "*** Push Public folder to GitHub Pages ***"
+# git push origin :gh-pages
+# if [[ $GH_TOKEN ]]; then
+# 	git subtree push --prefix=public https://$GH_TOKEN@github.com/marcotroisi/blog.git gh-pages > /dev/null 2>&1
+# else
+# 	git subtree push --prefix=public https://github.com/marcotroisi/blog.git gh-pages
+# fi
 
 echo "*** DONE! ***"
